9d00244ee34883d454adf47ee91ead0e42d675af
praful-cyber <mca20017@glbitm.ac.in>
2021-04-17 18:53:19 +0530

    do while loop.

--- conditional/loops.cpp
+++ conditional/loops.cpp
@@ -14,17 +14,29 @@
 // }
 
 /* while loop */
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+// 	int n;
+// 	while(n>0) {
+// 		cout<<"Enter a number: ";
+// 		cin>>n;
+// 		cout<<"You have entered : "<<n<<endl;
+// 	}
+// 	cout<<"You exit! "<<endl;
+// 	return 0;
+// }
 
+/* do while loop */
 #include<iostream>
 using namespace std;
 int main()
 {
 	int n;
-	while(n>0) {
-		cout<<"Enter a number: ";
+	do {
 		cin>>n;
-		cout<<"You have entered : "<<n<<endl;
-	}
-	cout<<"You exit! "<<endl;
+		cout<<n<<endl;
+	}while(n>0);
 	return 0;
 }
