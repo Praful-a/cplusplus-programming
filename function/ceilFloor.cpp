7cb58076d70313197b49e5367776d94d42866564
Praful <prafuldubey27@gmail.com>
2021-05-03 19:02:14 +0530

    Ceil and floor function

---
+++ function/ceilFloor.cpp
@@ -0,0 +1,23 @@
+// Program to demonstrate floor function
+#include<iostream>
+#include<cmath>
+using namespace std;
+/*
+int main()
+{
+	//using floor function which return 
+	// floor of input value
+	cout<< "Floor is : " << floor(2.3) << endl;
+	cout<< "Floor is : " << floor(-2.3) << endl;
+
+	return 0;
+}*/
+
+int main()
+{
+	// using ceil function which return
+	// floor of input value
+	cout << " Ceil is : " << ceil(2.3) << endl;
+	cout << " Ceil is : " << ceil(-2.3) << endl;
+	return 0;
+}
